0b209c1b69b577bcd48091b1061eac009950c3e9
peppko14 <hodea@pm.me>
2026-07-13 12:17:07 +0200

    Add stable burn identifiers

---
+++ history/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2026 peppko14
+# SPDX-License-Identifier: GPL-3.0-only
+
+"""Historienverwaltung für WiFire-Abbrände."""
+
+from .identifiers import build_burn_id, build_canonical_burn_text
+
+__all__ = ["build_burn_id", "build_canonical_burn_text"]
